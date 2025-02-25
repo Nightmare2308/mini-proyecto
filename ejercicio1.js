a089612a72940f1a91ac0deb8c04bedf8c6f176e
Nightmare2308 <leonelgilson43@gmail.com>
2025-02-24 21:13:37 -0400

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,23 @@
+
+
+
+// Función que muestra un mensaje en la página
+
+function mostrarMensajes() {
+
+    // Primer mensaje
+
+    alert("¡Hola! Este es un mensaje de JavaScript.");
+
+    
+
+    // Segundo mensaje
+
+    alert("Soy el primer script");
+
+}
+
+
+// Llamamos a la función para mostrar los mensajes
+
+mostrarMensajes();
